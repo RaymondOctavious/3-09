97cfab62a36c0e6378880a73d4e6e30e03150f77
Raymond <Raymond.octavious@ocsbstudent.ca>
2017-10-30 11:59:19 -0400

    Init

---
+++ 3-09.py
@@ -0,0 +1,12 @@
+# Created by raymond octavious
+# Created for ICS3U
+# Created on Oct 2017
+
+counter = 65
+counter2 = 97
+for counter in range(65, 91):
+    for counter2 in range(97, 122):
+        print(unichr(counter) + "->" + unichr(counter2))
+        
+    
+    
